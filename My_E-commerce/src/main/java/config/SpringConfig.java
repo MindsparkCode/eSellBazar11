09a32e15fd2ede6bdee8927d4c64c4feab8cfba3
G M Saiful Alam <59245133+SaifulAlam93@users.noreply.github.com>
2020-09-26 13:11:17 +0600

    Update SpringConfig.java
    config review

--- My_E-commerce/src/main/java/config/SpringConfig.java
+++ My_E-commerce/src/main/java/config/SpringConfig.java
@@ -49,6 +49,41 @@ public class SpringConfig implements WebMvcConfigurer {
 //   public void addRes
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+         registry.addResourceHandler("/*.html").addResourceLocations("/WEB-INF/html/*");
+        registry.addResourceHandler("/*.jsp").addResourceLocations("/WEB-INF/views/*");
+//        registry.addResourceHandler("/desh/").addResourceLocations("/WEB-INF/views/dashboard/*");
+        registry.addResourceHandler("/style.css").addResourceLocations("/WEB-INF/views/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/WEB-INF/bootcss/img/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/bootcss/css/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("/WEB-INF/bootcss/assets/");
+        registry.addResourceHandler("/rs-plugin/**").addResourceLocations("/WEB-INF/bootcss/rs-plugin/");
+        registry.addResourceHandler("/layerslider/**").addResourceLocations("/WEB-INF/bootcss/layerslider/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/bootcss/fonts/");
+        registry.addResourceHandler("/rs-plugin/**").addResourceLocations("/WEB-INF/bootcss/rs-plugin/");
+        //registry.addResourceHandler("/css/shortcode/**").addResourceLocations("/WEB-INF/css/shortcode/");
+        // registry.addResourceHandler("/images/shop/cart/**").addResourceLocations("/WEB-INF/images/shop/cart/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/");
+        registry.addResourceHandler("/js/vendor/**").addResourceLocations("/WEB-INF/bootcss/js/vendor/");
+        registry.addResourceHandler("/rs-plugin/js/**").addResourceLocations("/WEB-INF/bootcss/rs-plugin/js/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/bootcss/js/");
+        registry.addResourceHandler("/desh/dcss/img/**").addResourceLocations("/WEB-INF/dcss/img/");        
+         registry.addResourceHandler("/desh/dcss/css/**").addResourceLocations("/WEB-INF/dcss/css/");        
+        registry.addResourceHandler("/desh/dcss/js/**").addResourceLocations("/WEB-INF/dcss/js/");
+        registry.addResourceHandler("/desh/dcss/fonts/**").addResourceLocations("/WEB-INF/dcss/fonts/");
+        
+        
+        
+          registry.addResourceHandler("/dcss/img/**").addResourceLocations("/WEB-INF/dcss/img/");        
+         registry.addResourceHandler("/dcss/css/**").addResourceLocations("/WEB-INF/dcss/css/");        
+        registry.addResourceHandler("/dcss/js/**").addResourceLocations("/WEB-INF/dcss/js/");
+        registry.addResourceHandler("/dcss/fonts/**").addResourceLocations("/WEB-INF/dcss/fonts/");
+        
+        //registry.addResourceHandler("/*.css").addResourceLocations("/WEB-INF/css/");        
+        // registry.addResourceHandler("/*.xhtml").addResourceLocations("/WEB-INF/views/xhtml/");
+        // registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/fcss/js/*");               
+//registry.addResourceHandler("/acss/**").addResourceLocations("/WEB-INF/resources/css/*"); 
+
+
      
     }
 
